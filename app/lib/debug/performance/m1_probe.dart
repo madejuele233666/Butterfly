ca2b6cb0e4e42e3c590c8c70e153221214225ed2
madejuele <madejuele236@users.noreply.github.com>
2026-07-25 03:45:13 +0900

    style(m1): remove redundant Flutter imports

--- app/lib/debug/performance/m1_probe.dart
+++ app/lib/debug/performance/m1_probe.dart
@@ -2,8 +2,6 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:ui' show FramePhase, FrameTiming;
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
